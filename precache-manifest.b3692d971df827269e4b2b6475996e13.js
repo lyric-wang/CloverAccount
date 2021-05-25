71fd3f79ee63b11a27c0b9f9bc9df215b3bff1e8
lyric <yiyuanwang33@gmail.com>
2021-05-25 12:25:41 +0800

    deploy

---
+++ precache-manifest.b3692d971df827269e4b2b6475996e13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe15bb180d8305a8a7c",
+    "url": "/CloverAccount/css/app.c3f59b9f.css"
+  },
+  {
+    "revision": "225f1d13447d19ac549a74d372df781f",
+    "url": "/CloverAccount/img/qrcode.225f1d13.png"
+  },
+  {
+    "revision": "1d1657dcd833bd6491faa982c2182d58",
+    "url": "/CloverAccount/index.html"
+  },
+  {
+    "revision": "2fe15bb180d8305a8a7c",
+    "url": "/CloverAccount/js/app.1b3c5a67.js"
+  },
+  {
+    "revision": "406c091db3fc7323b3f9",
+    "url": "/CloverAccount/js/chunk-vendors.dded8866.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/CloverAccount/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/CloverAccount/robots.txt"
+  }
+]);
